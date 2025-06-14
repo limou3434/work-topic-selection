7188090963d94ca758a5d7972871a7b7acc03b1e
limou3434 <898738804@qq.com>
2025-06-14 23:50:26 +0800

    修改登陆后无法正确跳转主页的问题

--- client/work-topic-selection-react/src/pages/User/Login/index.tsx
+++ client/work-topic-selection-react/src/pages/User/Login/index.tsx
@@ -76,7 +76,7 @@ const Login: React.FC = () => {
         message.success(res.message);
         await fetchUserInfo();
         const urlParams = new URL(window.location.href).searchParams;
-        history.push(urlParams.get('redirect') || '/');
+        history.push('/' || urlParams.get('redirect'));
         refresh();
         return;
       }else if(res.code===50003){
