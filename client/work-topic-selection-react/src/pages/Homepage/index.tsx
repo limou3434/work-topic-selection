247b2347c5095ec74dd850757d8ceb1ce8343a4e
limou3434 <898738804@qq.com>
2025-06-18 00:44:04 +0800

    添加对不同端的使用说明，并且开始测试部署

--- client/work-topic-selection-react/src/pages/Homepage/index.tsx
+++ client/work-topic-selection-react/src/pages/Homepage/index.tsx
@@ -3,7 +3,7 @@ import { useModel } from '@umijs/max';
 import { Card, Divider, theme, Typography } from 'antd';
 import React from 'react';
 
-const { Title, Paragraph, Text, Link } = Typography;
+const { Title, Paragraph, Text } = Typography;
 
 /**
  * 每个单独的卡片，为了复用样式抽成了组件
@@ -108,8 +108,6 @@ const Welcome: React.FC = () => {
             backgroundPosition: '100% -30%',
             backgroundRepeat: 'no-repeat',
             backgroundSize: '274px auto',
-            backgroundImage:
-              "url('https://gw.alipayobjects.com/mdn/rms_a9745b/afts/img/A*BuFmQqsB2iAAAAAAAAAAAAAAARQnAQ')",
           }}
         >
           <p
@@ -132,7 +130,7 @@ const Welcome: React.FC = () => {
             <InfoCard
               index={1}
               href="https://www.nfu.edu.cn/"
-              title="了解 广州南方学院"
+              title="了解 广州南方学院学校"
               desc="广州南方学院（原中山大学南方学院）是 2006 年经教育部批准设立的综合性应用型普通本科高等学校，一直致力于建设国内特色..."
             />
             <InfoCard
@@ -152,39 +150,79 @@ const Welcome: React.FC = () => {
         <Divider />
         <Typography>
           <Title level={2}>1.如何使用?</Title>
-          <Paragraph>
-            作为学生该如何选题呢？
-          </Paragraph>
+          <Title level={3}>1.1.学生使用手册</Title>
+          <Paragraph>作为学生，您的主要任务就是</Paragraph>
           <Paragraph>
             <Text strong>
-              <ol>
+              <ul>
                 <li>
-                  打开冰箱
+                  <Text code>确认预选毕业设计题目</Text> 或 <Text code>取消预选毕业设计题目</Text>
                 </li>
                 <li>
-                  放入冰箱
+                  <Text code>确认提交毕业设计题目</Text> 或 <Text code>取消提交毕业设计题目</Text>
                 </li>
-                <li>
-                  关上冰箱
-                </li>
-              </ol>
+              </ul>
             </Text>
           </Paragraph>
+          <Paragraph>
+            学生应当在题目开放时间前，预选自己心仪的{' '}
+            <Text code>毕业设计题目（后续简称“题目”）</Text>
+            ，然后在题目开放时间内抢夺题目，每一个学生最终只能确认选择一个题目，并且在选择题目瞬间自动清空所有的预选清单，并且无法再次预选题目。
+          </Paragraph>
+          <Paragraph>
+            本系统 <Text mark>虽然提供取消当前已选题目的功能，但只根据学校的安排进行开放</Text>
+            。若无法取消当前已选题目，则需要联系自己当前已选题目的对应导师。
+          </Paragraph>
+          <Paragraph>
+            请不要过分使用某些连点器工具来使用本网站，
+            <Text style={{ color: 'red' }}>一旦被系统检测到账号异常行为将会进行临时封号</Text>
+            ，将严重导致您的选题安排。
+          </Paragraph>
+          图片演示...
+          <Title level={3}>1.2.教师使用手册</Title>
+          <Paragraph>作为教师，您的主要任务就是</Paragraph>
+          <Paragraph>
+            <Text strong>
+              <ul>
+                <li>
+                  <Text code>发布题目</Text> 或 <Text code>修改题目</Text>
+                </li>
+                <li>
+                  <Text code>查看题目状态</Text>
+                </li>
+                <li>
+                  <Text code>可以根据情况为已经审核通过的题目选择学生</Text>
+                </li>
+              </ul>
+            </Text>
+          </Paragraph>
+          图片演示...
+          <Title level={3}>1.3.主任使用手册</Title>
+          <Paragraph>作为主任，您的主要任务就是</Paragraph>
+          <Paragraph>
+            <Text strong>
+              <ul>
+                <li>
+                  <Text code>审核题目（通过 或 打回）</Text>
+                </li>
+                <li>
+                  <Text code>查看本系部学生的选题情况</Text>
+                </li>
+                <li>
+                  <Text code>快速导出选题情况表格文件</Text>
+                </li>
+              </ul>
+            </Text>
+          </Paragraph>
+          图片演示...
           <Title level={2}>2.遇到问题?</Title>
           <Paragraph>
-            如果您在使用的过程中遇到问题，先阅读下面常见的解决方案，尝试是否能够解决，如果实在遇到解决不了的问题可以发送邮箱到 <Text code>898738804@qq.com</Text>
+            如果您在使用的过程中遇到问题，先阅读下面常见的解决方案，尝试是否能够解决，如果实在遇到解决不了的问题可以发送邮箱到{' '}
+            <Text code>898738804@qq.com</Text>
           </Paragraph>
           <Paragraph>
             <ul>
-              <li>
-                <Link href="/docs/spec/proximity">Principles</Link>
-              </li>
-              <li>
-                <Link href="/docs/spec/overview">Patterns</Link>
-              </li>
-              <li>
-                <Link href="/docs/resources">Resource Download</Link>
-              </li>
+              <li>忘记密码、没有账号、无故封号？</li>
             </ul>
           </Paragraph>
         </Typography>
