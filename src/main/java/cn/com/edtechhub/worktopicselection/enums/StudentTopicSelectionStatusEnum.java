596636ff89ef746922933f83bfb0f1c9ee6c9831
limou3434 <898738804@qq.com>
2025-06-17 15:59:32 +0800

    添加对选题审核状态的枚举体表示

---
+++ src/main/java/cn/com/edtechhub/worktopicselection/enums/StudentTopicSelectionStatusEnum.java
@@ -0,0 +1,52 @@
+package cn.com.edtechhub.worktopicselection.enums;
+
+import cn.hutool.core.util.ObjUtil;
+import lombok.Getter;
+
+/**
+ * 学生专题选择状态枚举体
+ *
+ * @author <a href="https://github.com/limou3434">limou3434</a>
+ */
+@Getter
+public enum StudentTopicSelectionStatusEnum {
+
+    PRESELECT(0, "预先选择"),
+
+    SELECT(1, "确定选择"),
+    ;
+
+    /**
+     * 码值
+     */
+    private final int code;
+
+    /**
+     * 描述
+     */
+    private final String description;
+
+    /**
+     * 内部构造方法
+     */
+    StudentTopicSelectionStatusEnum(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    /**
+     * 根据码值获取枚举
+     */
+    public static StudentTopicSelectionStatusEnum getEnums(int code) { // TODO: 等待改正放回值为枚举
+        if (ObjUtil.isEmpty(code)) {
+            return null;
+        }
+        for (StudentTopicSelectionStatusEnum enumItem : StudentTopicSelectionStatusEnum.values()) {
+            if (enumItem.getCode() == code) {
+                return enumItem;
+            }
+        }
+        return null;
+    }
+
+}
