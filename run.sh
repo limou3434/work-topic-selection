9197999245923e264b6e1612fc259e1c6e3e1227
limou3434 <898738804@qq.com>
2025-06-19 13:06:13 +0800

    再次修改部署脚本方便部署

--- run.sh
+++ run.sh
@@ -6,12 +6,14 @@ git fetch origin && git reset --hard origin/main
 
 # 编译后端
 (
+  echo "后端编译..."
   ./mvnw clean package > server.log 2>&1
   echo "后端编译完成 ✅"
 ) &
 
 # 编译前端
 (
+  echo "前端编译..."
   cd ./client/work-topic-selection-react || exit 1
   yarn build > client.log 2>&1
   echo "前端编译完成 ✅"
@@ -22,12 +24,14 @@ wait
 
 # 部署服务
 (
-  sudo docker compose up -d --build work-topic-selection | tee server.log
+  echo "后端部署..."
+  sudo docker compose up -d --build work-topic-selection | sudo tee server.log > /dev/null
   echo "后端部署完成 ✅"
 ) &
 
 (
-  sudo docker compose up -d --build work-topic-selection-client | tee server.log
+  echo "前端部署..."
+  sudo docker compose up -d --build work-topic-selection-client | sudo tee client.log > /dev/null
   echo "前端部署完成 ✅"
 ) &
 
