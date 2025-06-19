425318e40c6ab90c3ea0dd69532214eb7e0ff616
limou3434 <898738804@qq.com>
2025-06-19 12:52:01 +0800

    提交部署脚本方便部署

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# 出错终止
+set -e
+
+# 编译后端
+(
+  ./mvnw clean package
+  echo "后端编译完成 ✅"
+) &
+
+# 编译前端
+(
+  cd ./client/work-topic-selection-react || exit 1
+  yarn build
+  echo "前端编译完成 ✅"
+) &
+
+# 等待完成
+wait
+
+# 项目部署
+(
+  sudo docker compose up -d --build work-topic-selection
+  echo "后端部署完成 ✅"
+) &
+
+(
+  sudo docker compose up -d --build work-topic-selection-client
+  echo "前端端部署完成 ✅"
+) &
