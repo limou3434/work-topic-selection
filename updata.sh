e58a52b9ee29047c7e4cd16c5daf1395f56107ef
limou3434 <898738804@qq.com>
2025-09-14 18:00:29 +0800

    修改脚本

--- updata.sh
+++ updata.sh
@@ -13,36 +13,28 @@
 set -e
 
 # 编译后端
-(
-  echo "后端编译..."
-  cd ./work-topic-selection-backend/ 
-  ./mvnw clean package > backend.log 2>&1
-  echo "后端编译完成 ✅"
-) &
+echo "后端编译..."
+cd ./work-topic-selection-backend/ 
+./mvnw clean package > backend.log 2>&1
+echo "后端编译完成 ✅"
 
 # 编译前端
-(
-  echo "前端编译..."
-  cd ./work-topic-selection-frontend/
-  npm i && npm run build > frontend.log 2>&1
-  echo "前端编译完成 ✅"
-) &
+echo "前端编译..."
+cd ./work-topic-selection-frontend/
+npm i && npm run build > frontend.log 2>&1
+echo "前端编译完成 ✅"
 
 # 等待编译
 wait
 
 # 部署服务
-(
-  echo "后端部署..."
-  sudo docker compose down work-topic-selection-frontend && sudo docker compose up -d --build work-topic-selection-frontend > /dev/null
-  echo "后端部署完成 ✅"
-) &
+echo "后端部署..."
+sudo docker compose down work-topic-selection-frontend && sudo docker compose up -d --build work-topic-selection-frontend > /dev/null
+echo "后端部署完成 ✅"
 
-(
-  echo "前端部署..."
-  sudo docker compose down work-topic-selection-backend && sudo docker compose up -d --build work-topic-selection-backend > /dev/null
-  echo "前端部署完成 ✅"
-) &
+echo "前端部署..."
+sudo docker compose down work-topic-selection-backend && sudo docker compose up -d --build work-topic-selection-backend > /dev/null
+echo "前端部署完成 ✅"
 
 # 等待部署
 wait
